8dd9a60e409ee832cf19f08317adfb080f123f87
omaremadx3 <omarahmedx3@gmail.com>
2023-10-06 13:44:51 -0700

    tesT

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,17 @@
  *  Return: Always 0 (Success)
  */
 int main(void)
-{
-	    int intType;
-	        float floatType;
-		    double doubleType;
-		        char charType;
-			long longtype;
+{ 
+int intType;
+float floatType;
+double doubleType;
+char charType;
+long longtype;
+long long longlongtype;
 
-	        printf("Size of int: %zu bytes\n", sizeof(int));
-		        printf("Size of int: %zu bytes\n", sizeof(float));
-			        printf("Size of int: %zu bytes\n", sizeof(double));
-				        printf("Size of int: %zu bytes\n", sizeof(char));
-					        printf("Size of int: %zu bytes\n", sizeof(long));
-						        return (0);
+ printf("Size of int: %zu bytes\n", sizeof(int));
+ printf("Size of int: %zu bytes\n", sizeof(long long));
+ printf("Size of int: %zu bytes\n", sizeof(char));
+ printf("Size of int: %zu bytes\n", sizeof(long))
+return (0);
 }
