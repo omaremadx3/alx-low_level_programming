ada06928730aa9ef6858702560de58fa973d351a
omaremadx3 <omarahmedx3@gmail.com>
2023-10-06 14:35:41 -0700

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long));
 printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
-printf ("Size of a float: %zu\n byte(s)", sizeof(float));
+printf("Size of a float: %zu\n byte(s)\n", sizeof(float));
 return (0);
 }
