845858e9f6eba53c0d9d33ee42960e74d3438a39
omaremadx3 <omarahmedx3@gmail.com>
2023-10-05 17:41:48 -0700

    3ady

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
-
 #include <stdio.h>
 
 /**
- *  * main - Entry point of the program
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *main - Entry point of the program
+ *
+ *Return: Always 0 (Success)
+ *
+ u*/
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+		/* printf replacment */
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
 }
-#include <stdio.h>
