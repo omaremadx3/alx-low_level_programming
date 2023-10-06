745bc3ff9fa6459b75a6807961aa2703cbfcfcc0
omaremadx3 <omarahmedx3@gmail.com>
2023-10-05 18:42:48 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,10 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * 
+ *
  */
 int main(void)
 {
-	char Message = "\"Programming is like building a multilingual puzzle\n";
-puts(Message); //prints message using puts
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
