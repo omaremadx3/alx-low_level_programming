407e06b1181cb73a0b1f12091b17ca56e67008d2
omaremadx3 <omarahmedx3@gmail.com>
2023-10-05 19:18:41 -0700

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,16 +4,12 @@
  *
  *  Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("Size of int: %zu bytes\n", sizeof(int));
 	printf("Size of int: %zu bytes\n", sizeof(float));
 	printf("Size of int: %zu bytes\n", sizeof(double));
 	printf("Size of int: %zu bytes\n", sizeof(char));
-	printf("Size of int: %zu bytes\n", sizeof(short));
-	printf("Size of int: %zu bytes\n", sizeof(long));
-	printf("Size of int: %zu bytes\n", sizeof(long long));
 	printf("Size of int: %zu bytes\n", sizeof(unsigned int));
-	printf("Size of int: %zu bytes\n", sizeof(unsigned char));
 	return (0);
 }
