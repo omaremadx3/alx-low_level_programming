a6320db8d7d3786cc4f94342beadb920528f239d
omaremadx3 <omarahmedx3@gmail.com>
2023-10-08 15:26:12 -0700

    test

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 16; n++)
+{
+if (n < 10)
+{
+putchar('0' + n);
+}
+else
+{
+putchar ('a' + n-10);
+}
+}
+putchar('\n');
+return (0);
+}
