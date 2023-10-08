465970799924223fb07b8bcf32258b5c0f6d7a2e
omaremadx3 <omarahmedx3@gmail.com>
2023-10-08 14:48:43 -0700

    tes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints letters form a to z
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
