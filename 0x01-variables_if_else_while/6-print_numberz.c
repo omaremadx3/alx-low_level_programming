7d649de726b654802e31495dacdf782fa4c307e5
omaremadx3 <omarahmedx3@gmail.com>
2023-10-08 14:40:14 -0700

    test

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar('0' + n)
+}
+putchar('\n');
+return (0);
+}
