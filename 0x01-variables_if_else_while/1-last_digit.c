55629e16031ab475c434346c5c0b71f08d237807
omaremadx3 <omarahmedx3@gmail.com>
2023-10-08 13:09:39 -0700

    test

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ printf("Last digit of %d is %d and is 0\n", n, LastDigit);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n,o LastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LastDigit);
 }
 return (0);
 }
