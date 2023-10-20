dc9f7af229e96eaabb2dbab07eed593be022a226
omaremadx3 <omarahmedx3@gmail.com>
2023-10-19 20:28:15 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,18 +5,18 @@
  * @s2: 2nd string
  * Return: 0 less than 0 or greater
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *n1, char *n2)
 {
 int i = 0, y, z;
-while (s1[i] != '\0' || s2[i] != '\0')
+while (n1[i] != '\0' || n2[i] != '\0')
 {
 i++;
 }
 for (y = 0; y < i; y++)
 {
-if (s1[y] != s2[y])
+if (n1[y] != n2[y])
 {
-z = s1[y] - s2[y];
+z = n1[y] - n2[y];
 return (z);
 }
 }
